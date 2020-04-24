40a9f105739d77c224a4e20518eb477913492e66
Tomas Luna <tomasluna1996@gmail.com>
2020-04-24 19:26:57 -0300

    init commit

---
+++ src/main/java/viejo/luna/threadsandsem/ThreadsandsemApplication.java
@@ -0,0 +1,13 @@
+package viejo.luna.threadsandsem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThreadsandsemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThreadsandsemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/viejo/luna/threadsandsem/ThreadsandsemApplicationTests.java
@@ -0,0 +1,13 @@
+package viejo.luna.threadsandsem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadsandsemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
